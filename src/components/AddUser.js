a4790df547d1aef80b817378a595888b822a4d6f
Asraful Islam <76778254+iasraful@users.noreply.github.com>
2023-02-21 20:21:35 +0600

    Update AddUser.js

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -45,7 +45,7 @@ export default function AddUser() {
     { label: "Milk & Dairy Products", value: "milk & dairy products" },
   ];
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-100 px-6 lg:px-8 h-screen">
+    <div className="flex flex-col items-center bg-gray-100 px-6 lg:px-8 h-screen">
       <img
         className="mx-auto h-16 w-auto"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
